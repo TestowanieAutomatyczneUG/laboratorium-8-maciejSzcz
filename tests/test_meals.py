66e942fa474d1007a9c6632c5d749226608c9da0
MaciejSzczesny <yetek.lol@gmail.com>
2020-11-24 17:37:47 +0100

    added more specific test cases

--- tests/test_meals.py
+++ tests/test_meals.py
@@ -7,13 +7,19 @@ class TestMeal(unittest.TestCase):
         self.temp = Meal()
 
     def test_get_meal_by_name(self):
-        self.assertGreater(len(self.temp.get_meal_by_name("spaghetti")), 0)
+        self.assertGreater(len(self.temp.get_meal_by_name("spaghetti")["meals"]), 0)
+
+    def test_get_meal_by_name_correct_id(self):
+        self.assertEqual(self.temp.get_meal_by_name("spaghetti")["meals"][0]["idMeal"], "52770")
 
     def test_get_meal_by_name_input_not_str(self):
         self.assertRaises(TypeError, self.temp.get_meal_by_name, 12)
 
     def test_get_meal_by_first_letter(self):
-        self.assertGreater(len(self.temp.get_meal_by_first_letter("a")), 0)
+        self.assertGreater(len(self.temp.get_meal_by_first_letter("a")["meals"]), 0)
+
+    def test_get_meal_by_first_letter_correct_id(self):
+        self.assertEqual(self.temp.get_meal_by_first_letter("a")["meals"][0]["idMeal"], "52768")
 
     def test_get_meal_by_first_letter_too_long_input(self):
         self.assertRaises(Exception, self.temp.get_meal_by_first_letter, "ab")
@@ -22,28 +28,37 @@ class TestMeal(unittest.TestCase):
         self.assertRaises(TypeError, self.temp.get_meal_by_first_letter, 4532+1j)
 
     def test_get_meal_details_by_id_int_input(self):
-        self.assertGreater(len(self.temp.get_meal_details_by_id(52772)), 0)
+        self.assertGreater(len(self.temp.get_meal_details_by_id(52772)["meals"]), 0)
+
+    def test_get_meal_details_by_id_correct_name(self):
+        self.assertEqual(self.temp.get_meal_details_by_id(52772)["meals"][0]["strMeal"], "Teriyaki Chicken Casserole")
 
     def test_get_meal_details_by_id_str_input(self):
-        self.assertGreater(len(self.temp.get_meal_details_by_id("52772")), 0)
+        self.assertGreater(len(self.temp.get_meal_details_by_id("52772")["meals"]), 0)
     
     def test_get_meal_details_by_id_invalid_input(self):
         self.assertRaises(TypeError, self.temp.get_meal_details_by_id, 12321+2j)
 
     def test_get_random_meal(self):
-        self.assertGreater(len(self.temp.get_random_meal()), 0)
+        self.assertGreater(len(self.temp.get_random_meal()["meals"]), 0)
 
     def test_get_meal_categories(self):
         self.assertEqual(len(self.temp.get_meal_categories()["categories"]), 14)
 
     def test_filter_by_category(self):
-        self.assertGreater(len(self.temp.filter_by_category('Seafood')), 0)
+        self.assertGreater(len(self.temp.filter_by_category('Seafood')["meals"]), 0)
+
+    def test_filter_by_category_correct_id(self):
+        self.assertEqual(self.temp.filter_by_category('Seafood')["meals"][0]["idMeal"], "52959")
 
     def test_filter_by_category_wrong_input(self):
         self.assertRaises(TypeError, self.temp.filter_by_category, 123124325324)
     
+    def test_filter_by_area_correct_id(self):
+        self.assertEqual(self.temp.filter_by_area('Canadian')["meals"][0]["idMeal"], "52928")
+
     def test_filter_by_area(self):
-        self.assertGreater(len(self.temp.filter_by_area('Canadian')), 0)
+        self.assertGreater(len(self.temp.filter_by_area('Canadian')["meals"]), 0)
 
     def test_filter_by_area_wrong_input(self):
         self.assertRaises(TypeError, self.temp.filter_by_area, 5432668323)
