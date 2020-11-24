8d42a4cb444b78562786024f5fe4308ef7792bcc
MaciejSzczesny <yetek.lol@gmail.com>
2020-11-24 17:25:02 +0100

    implemented api calls and basics tests

--- src/meals/meals.py
+++ src/meals/meals.py
@@ -2,12 +2,23 @@ import requests
 
 class Meal:
     def get_meal_by_name(self, name):
+        if type(name) != str:
+            raise TypeError("Input must be of string type!")
+
         return requests.get(f'https://www.themealdb.com/api/json/v1/1/search.php?s={name}').json()
 
     def get_meal_by_first_letter(self, letter):
+        if len(letter) > 1:
+            raise Exception("Input must be a single letter")
+        elif type(letter) != str:
+            raise TypeError("Input must be of string type!")
+
         return requests.get(f'https://www.themealdb.com/api/json/v1/1/search.php?f={letter}').json()
     
     def get_meal_details_by_id(self, id_value):
+        if type(id_value) != str and type(id_value) != int:
+            raise TypeError("Input must be of string or integer type!")
+
         return requests.get(f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={id_value}').json()
 
     def get_random_meal(self):
@@ -17,7 +28,13 @@ class Meal:
         return requests.get('https://www.themealdb.com/api/json/v1/1/categories.php').json()
 
     def filter_by_category(self, category):
+        if type(category) != str:
+            raise TypeError("Input must be of string type!")
+
         return requests.get(f'https://www.themealdb.com/api/json/v1/1/filter.php?c={category}').json()
     
     def filter_by_area(self, area):
+        if type(area) != str:
+            raise TypeError("Input must be of string type!")
+
         return requests.get(f'https://www.themealdb.com/api/json/v1/1/filter.php?a={area}').json()
--- tests/test_meals.py
+++ tests/test_meals.py
@@ -9,23 +9,44 @@ class TestMeal(unittest.TestCase):
     def test_get_meal_by_name(self):
         self.assertGreater(len(self.temp.get_meal_by_name("spaghetti")), 0)
 
+    def test_get_meal_by_name_input_not_str(self):
+        self.assertRaises(TypeError, self.temp.get_meal_by_name, 12)
+
     def test_get_meal_by_first_letter(self):
         self.assertGreater(len(self.temp.get_meal_by_first_letter("a")), 0)
 
-    def test_get_meal_details_by_id(self):
+    def test_get_meal_by_first_letter_too_long_input(self):
+        self.assertRaises(Exception, self.temp.get_meal_by_first_letter, "ab")
+
+    def test_get_meal_by_first_letter_input_not_str(self):
+        self.assertRaises(TypeError, self.temp.get_meal_by_first_letter, 4532+1j)
+
+    def test_get_meal_details_by_id_int_input(self):
         self.assertGreater(len(self.temp.get_meal_details_by_id(52772)), 0)
+
+    def test_get_meal_details_by_id_str_input(self):
+        self.assertGreater(len(self.temp.get_meal_details_by_id("52772")), 0)
+    
+    def test_get_meal_details_by_id_invalid_input(self):
+        self.assertRaises(TypeError, self.temp.get_meal_details_by_id, 12321+2j)
 
     def test_get_random_meal(self):
         self.assertGreater(len(self.temp.get_random_meal()), 0)
 
     def test_get_meal_categories(self):
-        self.assertGreater(len(self.temp.get_meal_categories()), 0)
+        self.assertEqual(len(self.temp.get_meal_categories()["categories"]), 14)
 
     def test_filter_by_category(self):
         self.assertGreater(len(self.temp.filter_by_category('Seafood')), 0)
+
+    def test_filter_by_category_wrong_input(self):
+        self.assertRaises(TypeError, self.temp.filter_by_category, 123124325324)
     
     def test_filter_by_area(self):
         self.assertGreater(len(self.temp.filter_by_area('Canadian')), 0)
+
+    def test_filter_by_area_wrong_input(self):
+        self.assertRaises(TypeError, self.temp.filter_by_area, 5432668323)
 
     def tearDown(self):
         self.temp = None
